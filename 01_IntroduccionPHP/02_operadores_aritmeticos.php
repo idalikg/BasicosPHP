4ff496706b004e680e9fe94ad08c70c822268b4e
Dalia Galván <idalikg@gmail.com>
2018-01-14 12:43:27 -0600

    Operadores Aritmeticos Ejemplo del uso de operadores de suma, resta, multiplicación, división y módulo.

---
+++ 01_IntroduccionPHP/02_operadores_aritmeticos.php
@@ -0,0 +1,28 @@
+<?php
+    /**
+     * + suma
+     * - resta
+     * * multiplicación
+     * % módulo
+     */
+
+    $var1 = 5;
+    $var2 = 3;
+    $var3 = 10;
+
+    $suma = $var1 + $var2 + $var3; // 5+23+10
+
+    $resta = $var3 - $var2; // 10-3
+
+    $multiplicacion = $var1 * $var1; // 5*5
+
+    $division = $var3 / $var1;
+
+    $modulo = $var3 / $var1;
+
+    echo $suma . PHP_EOL; // 18
+    echo $resta . PHP_EOL; // 7
+    echo $multiplicacion . PHP_EOL;
+    echo $division . PHP_EOL;
+    echo $modulo . PHP_EOL;
+?>
